9f055f0f0be6ad76750d0cc91a397a96913136cb
Fernando Villanueva <fernandoe.villanueval@hotmail.com>
2022-06-14 01:35:24 -0600

    :sparkles: implement drag n drop functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
+import { Task } from './interfaces/task.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'drag-and-drop';
+  toDos: Task[] = [
+    {
+      id: 1,
+      title: 'Learn Angular',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'Learn TypeScript',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Learn Angular Material',
+      completed: false,
+    },
+    {
+      id: 4,
+      title: 'Learn Angular Router',
+      completed: false,
+    },
+  ];
+  completed: Task[] = [
+    {
+      id: 5,
+      title: 'Learn React',
+      completed: true,
+    },
+  ];
+
+  constructor() {}
+
+  drop(event: CdkDragDrop<Task[]>): void {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(this.toDos, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+
+    console.log(this.toDos, this.completed);
+  }
 }
